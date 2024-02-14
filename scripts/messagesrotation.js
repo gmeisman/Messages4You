435b55f508f9146ef5760bee53df03ff4f039f05
GarrenBYU <mei21004@byui.edu>
2024-02-14 12:26:48 -0700

    I got the automated random messages and the dates working

--- scripts/messagesrotation.js
+++ scripts/messagesrotation.js
@@ -6,14 +6,15 @@ function getRandomElement(array) {
 let quotes = ['"The best thing to hold onto in life is each other." - Audrey Hepburn', '"Where this is love ther is life." -Mahatma Gandhi', '"I have decided to stick with love. Hate is too great a burden to bear." - Martin Luther King, Jr.', '"We are shaped and fashioned by those we love." - Goethe', '"Love is not only something you feel, it is something yoiu do." - David Wilkerson']
 let chosenQuote = "empty";
 let currentDate = new Date();
+currentDate.setHours(0, 0, 0, 0);
 let specificDate = new Date(2024, 1, 14)
-if(specificDate == currentDate){
+specificDate.setHours(0, 0, 0, 0)
+if(specificDate.getDate() == currentDate.getDate()){
     chosenQuote = "Happy Valentines Day! 💓"
 }
 else{
-    chosenQuote = "Happy Valentines Day! 💓"
+    chosenQuote = getRandomElement(quotes)
 }
-//getRandomElements(quotes)
 
 let elementContainer = document.getElementById("message");
 elementContainer.textContent = chosenQuote.toString()
