7b760db3de770d87c22ffc676dcaaf1dd4349c3c
GarrenBYU <mei21004@byui.edu>
2024-02-16 12:26:17 -0700

    I got it to update based on a txt file

--- scripts/messagesrotation.js
+++ scripts/messagesrotation.js
@@ -5,7 +5,48 @@ function getRandomElement(array) {
 
 let OGquotes = ['"The best thing to hold onto in life is each other." - Audrey Hepburn', '"Where this is love ther is life." -Mahatma Gandhi', '"I have decided to stick with love. Hate is too great a burden to bear." - Martin Luther King, Jr.', '"We are shaped and fashioned by those we love." - Goethe', '"Love is not only something you feel, it is something yoiu do." - David Wilkerson']
 
-function runAtMidnight(){
+let date = null
+let message = null
+
+function getDate(message){
+    fetch('date.txt')
+        .then(response => response.text())
+        .then(data => {
+            date = data;
+            let elementContainer = document.getElementById("message");
+            elementContainer.textContent = message.toString()
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+fetch('messages.txt')
+    .then(response => response.text())
+    .then(data => {
+        message = data;
+        getDate(message)
+        console.log(message);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+function getChosenQuote(message, date){
+    let quotes = ['"The best thing to hold onto in life is each other." - Audrey Hepburn', '"Where this is love ther is life." -Mahatma Gandhi', '"I have decided to stick with love. Hate is too great a burden to bear." - Martin Luther King, Jr.', '"We are shaped and fashioned by those we love." - Goethe', '"Love is not only something you feel, it is something yoiu do." - David Wilkerson']
+    let chosenQuote = "empty";
+    let currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    let specificDate = new Date(2024, 1, 14)
+    specificDate.setHours(0, 0, 0, 0)
+    if(specificDate.getDate() == currentDate.getDate()){
+        chosenQuote = "Happy Valentines Day! 💓"
+    }
+    else{
+        chosenQuote = getRandomElement(quotes)
+    }
+}
+/*function runAtMidnight(){
     let quotes = ['"The best thing to hold onto in life is each other." - Audrey Hepburn', '"Where this is love ther is life." -Mahatma Gandhi', '"I have decided to stick with love. Hate is too great a burden to bear." - Martin Luther King, Jr.', '"We are shaped and fashioned by those we love." - Goethe', '"Love is not only something you feel, it is something yoiu do." - David Wilkerson']
     let chosenQuote = "empty";
     let currentDate = new Date();
@@ -28,10 +69,14 @@ function timeTillMidnight() {
         0, 0, 0
     );
     return midnight - now;
-}
+}*/
+
+/*
 setTimeout(runAtMidnight, timeTillMidnight());
 let elementContainer = document.getElementById("message");
-elementContainer.textContent = chosenQuote.toString()
+elementContainer.textContent = message.toString()*/
+console.log(message);
+
 //Copywrite Year
 /*setElementText(".Message", (chosenQuote).toString());
 
