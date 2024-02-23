7c2d5e34d63b47503836c8b37ffe01a7f1135f5f
GarrenBYU <mei21004@byui.edu>
2024-02-23 10:30:15 -0700

    new updates

--- scripts/messagesrotation.js
+++ scripts/messagesrotation.js
@@ -47,7 +47,8 @@ function getChosenQuote(message, date){
     }
 }
 
-//This is going to be the part that writes to the txt files
+//This is going to be the part that writes to the txt files using a txt file, but I couldnt get it to update the txt file
+/*
 function updateFiles(file){
     const GitHubAPI = 'https://api.github.com/repos/gmeisman/Messages4You/contents/messages.txt';
     fetch(GitHubAPI, {
@@ -80,22 +81,53 @@ function updateFiles(file){
     .then(data => console.log(data));
 }
 
-updateFiles('messages.txt');
-/*function runAtMidnight(){
-    let quotes = ['"The best thing to hold onto in life is each other." - Audrey Hepburn', '"Where this is love ther is life." -Mahatma Gandhi', '"I have decided to stick with love. Hate is too great a burden to bear." - Martin Luther King, Jr.', '"We are shaped and fashioned by those we love." - Goethe', '"Love is not only something you feel, it is something yoiu do." - David Wilkerson']
+updateFiles('messages.txt');*/
+
+function runAtMidnight(){
+    let quotes = ['I still remember sitting in a car with a switch and rocket league. I remember being so nervous I didnt think I could ever do anything again. I didnt know if I had to poop, pee, or to just lean over and take your hand. The following days when we leaned on eachother at Craigs place made me more giddy than any other feeling in the world.', '"Where this is love ther is life." -Mahatma Gandhi', '"I have decided to stick with love. Hate is too great a burden to bear." - Martin Luther King, Jr.', '"We are shaped and fashioned by those we love." - Goethe', '"Love is not only something you feel, it is something yoiu do." - David Wilkerson']
     let chosenQuote = "empty";
     let currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
-    let specificDate = new Date(2024, 1, 14)
+    let specificDate0 = new Date(2024, 1, 22)
     specificDate.setHours(0, 0, 0, 0)
-    if(specificDate.getDate() == currentDate.getDate()){
-        chosenQuote = "Happy Valentines Day! 💓"
+    let specificDate1 = new Date(2024, 1, 23)
+    specificDate.setHours(0, 0, 0, 0)
+    let specificDate2 = new Date(2024, 1, 24)
+    specificDate.setHours(0, 0, 0, 0)
+    let specificDate3 = new Date(2024, 1, 25)
+    specificDate.setHours(0, 0, 0, 0)
+    let specificDate4 = new Date(2024, 1, 26)
+    specificDate.setHours(0, 0, 0, 0)
+    let specificDate5 = new Date(2024, 1, 27)
+    specificDate.setHours(0, 0, 0, 0)
+    let specificDate6 = new Date(2024, 1, 28)
+    specificDate.setHours(0, 0, 0, 0)
+    if(specificDate0.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[0];
     }
-    else{
+    else if(specificDate1.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[1];
+    }
+    else if(specificDate2.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[2];
+    }
+    else if(specificDate3.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[3];
+    }
+    else if(specificDate4.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[4];
+    }
+    else if(specificDate5.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[5];
+    }
+    else if(specificDate6.getDate() == currentDate.getDate()){
+        chosenQuote = quotes[6];
+    }
+    /*else{
         chosenQuote = getRandomElement(quotes)
-    }
+    }*/
 }
-
+/*
 function timeTillMidnight() {
     const midnight = new Date(
         now.getFullYear(),
@@ -110,11 +142,11 @@ function timeTillMidnight() {
 setTimeout(runAtMidnight, timeTillMidnight());
 let elementContainer = document.getElementById("message");
 elementContainer.textContent = message.toString()*/
-console.log(message);
+console.log(chosenQuote);
 
 //Copywrite Year
-/*setElementText(".Message", (chosenQuote).toString());
-
+setElementText(".Message", (chosenQuote).toString());
+/*
 function setElementText(element, text){
     document.querySelector(element).innerText = text;
 }*/
